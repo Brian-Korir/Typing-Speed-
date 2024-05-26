8cdd662419b178c03f4b3093e30a81ff0693688b
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-26 16:38:50 +0300

    Update type.py

--- type.py
+++ type.py
@@ -2,4 +2,5 @@ import time
 from pyuca import Collator
 #Intro message
 print("Welcome to the typing  test!")
-
+print("Type your own words and press enter to check your results")
+print("We will check your speling too!")
