dda61aa87485de896390f2c29ddf6b915263e614
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-26 16:34:29 +0300

    Update type.py

--- type.py
+++ type.py
@@ -1,7 +1,1 @@
 import time
-def calculate speed (start_time, end_time, characters):
-#calculate elapsed time in seconds
-elapsed_time = start_time - end_time
-#Remove trailing new line character
-characters =  characters.strip()
-word_count = len (characters.split())
