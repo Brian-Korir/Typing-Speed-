fc06ba5d5e8be36996914bb3e1393d969d2f59d1
Brian Korir <144766290+Brian-Korir@users.noreply.github.com>
2024-05-26 16:49:41 +0300

    Update type.py

--- type.py
+++ type.py
@@ -3,13 +3,27 @@ import random
 from pyuca import Collator
 def get_random_sentence():
   sentences=[
+    
        "The quick brown fox jumps over the lazy dog.",
       "A programmer is a person who can fix a problem without knowing what it is.",
       "Python is a powerful and versatile programming language.",
       "There are 10 types of people in the world: those who understand binary, and those who don't.",
       "Always code as if the person who ends up maintaining your code will be a violent psychopath who knows where you live.",
   ]
+  return random.choice(sentences)
+def typing_test():
+  
 #Intro message
 print("Welcome to the typing  test!")
 print("Type your own words and press enter to check your results")
 print("We will check your speling too!")
+#gets random sentence 
+random_sentence = get_random_sentence()
+print(random-sentence)
+
+#start time 
+start_time = time.time()
+
+#end time 
+end-time = time.time()
+
